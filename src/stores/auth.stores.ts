bfc1588c5af576368ef98387b4937cc0a0da8517
Camil Saunier <camilsaunier@gmail.com>
2026-09-01 14:45:46 +0200

    feat(client): add auth store and set up stores directory

---
+++ src/stores/auth.stores.ts
@@ -0,0 +1,17 @@
+// src/stores/auth.store.ts
+import { create } from "zustand";
+import type { User } from "../types";
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  setAuth: (user: User, token: string) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: null,
+  setAuth: (user, token) => set({ user, token }),
+  logout: () => set({ user: null, token: null }),
+}));
